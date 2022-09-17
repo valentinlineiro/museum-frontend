66327cc09c46483093442780c79b196185b6b3e1
valentinlineiro <valentinlineiro@gmail.com>
2022-09-17 14:30:39 +0200

    Centering empty state

--- src/app/features/halloffame/service/in-memory-halloffame.service.ts
+++ src/app/features/halloffame/service/in-memory-halloffame.service.ts
@@ -10,6 +10,6 @@ export class InMemoryHalloffameService implements HalloffameService {
   constructor() {}
 
   list(): Observable<Entry[]> {
-    return of(this.entries).pipe(delay(5000));
+    return of(this.entries).pipe(delay(250));
   }
 }
--- src/app/shared/component/button/fab/fab.component.ts
+++ src/app/shared/component/button/fab/fab.component.ts
@@ -1,8 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-fab',
   templateUrl: './fab.component.html',
   styleUrls: ['./fab.component.css'],
 })
-export class FabComponent {}
+export class FabComponent {
+  @Input() color: string = 'primary';
+  @Input() icon: string = 'add';
+  @Output() click: EventEmitter<any> = new EventEmitter<any>();
+
+  onClick(): void {
+    this.click.emit();
+  }
+}
