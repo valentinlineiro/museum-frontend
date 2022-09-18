8616a1180413f2801949788b272d9dbb635bfcdb
valentinlineiro <valentinlineiro@gmail.com>
2022-09-18 10:32:45 +0200

    Adding form control

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -20,6 +21,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatTableModule,
     MatCardModule,
     MatProgressSpinnerModule,
+    MatFormFieldModule,
   ],
 })
 export class MaterialModule {}
